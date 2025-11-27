6fa6a1723b7308932eea384d12bcf7cd7920bfc6
weeb-bwee <weeebeee1011@gmail.com>
2025-11-27 12:01:39 +0700

    test

---
+++ wildan.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void XorGate() {
+    cout << "Tabel Kebenaran XOR\n";
+    cout << left << setw(5) << "B" << setw(5) << "A" << " | " << "Output\n";
+    cout << "------------------------\n";
+    for (int i = 0; i <= 1; i++) {
+        for (int j = 0; j <= 1; j++) {
+            cout << left << setw(5) << i << setw(5) << j << " | " << (i ^ j) << endl;
+        }
+    }
+}
+
+void XnorGate() {
+    cout << "Tabel Kebenaran XNOR\n";
+    cout << left << setw(5) << "B" << setw(5) << "A" << " | " << "Output\n";
+    cout << "------------------------\n";
+    for (int i = 0; i <= 1; i++) {
+        for (int j = 0; j <= 1; j++) {
+            cout << left << setw(5) << i << setw(5) << j << " | " << !(i ^ j) << endl;
+        }
+    }
+}
+
+int main() {
+    int input;
+    char answer;
+
+    cout << "Selamat Datang Di Pusat Informasi Tabel Kebenaran";
+
+jump:  cout << "\n\n1. AND\n2. OR\n3. NOT\n4. NAND\n5. NOR\n6. XOR\n7. XNOR\n";
+    cout << "Silahkan Pilih Gerbang Logika yang Anda Inginkan : ";
+    cin >> input;
+    cout << "\n\n";
+
+    switch (input) {
+    case 6:
+        XorGate();
+        cout << "\nApakah Anda Ingin Melihat Tabel Kebenaran Dari Gerbang Logika Lainnya ? (Y/N) : ";
+        cin >> answer;
+        while (answer == 'Y' || answer == 'y') {
+            goto jump;
+        }; break;
+    case 7:
+        XorGate();
+        cout << "\nApakah Anda Ingin Melihat Tabel Kebenaran Dari Gerbang Logika Lainnya ? (Y/N) : ";
+        cin >> answer;
+        while (answer == 'Y' || answer == 'y') {
+            goto jump;
+        }; break;
+    default: cout << "Pilihan Anda Tidak Tersedia"; break;
+    }
+}
